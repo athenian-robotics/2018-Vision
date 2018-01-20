ac660eca12c895f7109471b0f5e4fc43598210c1
jacksonmoffet <jrmoffet@gmail.com>
2018-01-20 14:39:23 -0800

    Initial Commit

---
+++ LineOrientation.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(1)
+# TODO actually find lines, this is just boiler plate
+
+while 1:
+    ret, frame = cap.read()
+    cv2.imshow('Frame', cv2.flip(frame, 1))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
